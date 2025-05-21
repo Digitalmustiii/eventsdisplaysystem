3232de379bd0780693e187f7f87d7d8e0188ac4d
Digitalmustiii <sanusimustapha387@gmail.com>
2025-05-21 08:36:45 +0100

    Embed OpenWeather key in next.config.js to avoid Vercel env issues

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_OPENWEATHER_API: '578de1c8e2454537d559afebacf6eab6'
+  }
+}
+
+module.exports = nextConfig
