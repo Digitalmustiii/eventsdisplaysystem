c84e1d9965d1188e8072031aad3c05baf23c14b6
Digitalmustiii <sanusimustapha387@gmail.com>
2025-05-21 03:19:59 +0100

    lol

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-// Root layout component that would typically be in app/layout.tsx or similar
-// This shows how the components should be arranged
+// app/page.tsx
+import Header from '@/app/components/Header'
+import Body from '@/app/components/Body'
 
-'use client'
-
-import Header from './components/Header'
-import Body from './components/Body'
-
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Page() {
   return (
-    <div className="flex flex-col h-screen w-full">
-      {/* No margin or padding between components */}
+    <>
       <Header />
       <Body />
-      {children}
-    </div>
+    </>
   )
 }
