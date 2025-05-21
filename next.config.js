8a31d2dbc53f5b99e1418a2b42aa0688ab1f77e6
Digitalmustiii <sanusimustapha387@gmail.com>
2025-05-21 08:51:32 +0100

    lol

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_OPENWEATHER_API: '578de1c8e2454537d559afebacf6eab6'
-  }
-}
-
-module.exports = nextConfig
